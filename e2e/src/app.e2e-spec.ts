785bcc6f8a9518acb3a4e91d1d018a13f191830d
Jripolles <juanripoca@hotmail.com>
2021-03-26 08:00:34 +0100

    add e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -7,8 +7,44 @@ describe('new App', () => {
     page = new AppPage();
   });
 
-  it('should be blank', () => {
+  it('should have all elemets in the page', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toContain('Start with Ionic UI Components');
+    const pageElements = [
+      page.getInputEmail(),
+      page.getInputPass(),
+      page.geButtonSubmit(),
+      page.getAvatar(),
+      page.getReminderToogle(),
+      page.getIconPerson(),
+      page.getIconLock()
+    ];
+    pageElements.forEach((pageElement) => {
+      expect(pageElement).toBeDefined();
+    })
+    expect(page.getParagraphText()).toContain('Prueba técnica');
+  });
+
+  it('OK Scenario - should not show errors if is filled correcly', async () => {
+    await page.navigateTo();
+    const inputEmail = page.getInputEmail();
+    const inputPass = page.getInputPass();
+    const submitButton = page.geButtonSubmit();
+    await inputEmail.sendKeys('correct@email.com');
+    await inputPass.sendKeys('okoko');
+    await submitButton.click();
+    const errors = page.getErrorElements();
+    expect((await errors).length).toBe(0)
+  });
+
+  it('KO Scenario - should show errors if is filled with erros', async () => {
+    await page.navigateTo();
+    const inputEmail = page.getInputEmail();
+    const inputPass = page.getInputPass();
+    const submitButton = page.geButtonSubmit();
+    await inputEmail.sendKeys('incorrect email');
+    await inputPass.sendKeys('ko');
+    await submitButton.click();
+    const errors = await  page.getErrorElements();
+    expect(errors.length).toBe(2)
   });
 });
--- e2e/src/app.po.ts
+++ e2e/src/app.po.ts
@@ -1,11 +1,43 @@
-import { browser, by, element } from 'protractor';
+import { browser, by, element, ElementArrayFinder, ElementFinder } from 'protractor';
 
 export class AppPage {
   navigateTo() {
-    return browser.get('/');
+    return browser.get('/home');
+  }
+
+  getAvatar(): ElementFinder {
+    return element(by.css('ion-avatar'));
+  }
+
+  getReminderToogle(): ElementFinder {
+    return element(by.css('login__toogle'));
+  }
+
+  getIconPerson(): ElementFinder {
+    return element(by.css('ion-icon[name="person-outline"]'));
+  }
+
+  getIconLock(): ElementFinder {
+    return element(by.css('ion-icon[name="lock-closed-outline"]'));
+  }
+
+  geButtonSubmit() {
+    return element(by.css('button'));
   }
 
   getParagraphText() {
     return element(by.deepCss('app-root ion-content')).getText();
+  }
+
+  getInputEmail(): ElementFinder {
+    return element(by.css('input[type="email"]'));
+  }
+
+  getInputPass(): ElementFinder {
+    return element(by.css('input[type="password"]'));
+  }
+
+  getErrorElements(): ElementArrayFinder {
+    return element.all(by.css('.login__error'));
   }
 }
