44ce157151492edb3c0e04eee729bd07bbe1c9fe
Alejo Statti <alejo.statti@gmail.com>
2021-07-02 02:12:11 +0200

    Menu con click slide

--- src/app/components/market/market.component.ts
+++ src/app/components/market/market.component.ts
@@ -2,7 +2,7 @@ import { Component,ElementRef,OnInit,ViewChild,} from '@angular/core';
 import { ProductosService} from '../../services/productos.service';
 import { IMG_URL} from '../market/constantes';
 import { Router, ActivatedRoute} from '@angular/router';
-import { ModalController} from '@ionic/angular';
+import { MenuController, ModalController} from '@ionic/angular';
 import {IdproductComponent} from '../idproduct/idproduct.component';
 import {  CartComponent } from '../cart/cart.component';
 import { StorageCartService  } from 'src/app/services/storageCart.service';
@@ -19,9 +19,16 @@ export class marketComponent implements OnInit {
   cartArray : any;
   cartItemCount: BehaviorSubject<number>;
 
+  menuOpen: boolean = true;
+
   @ViewChild('cart', {static: false, read: ElementRef})fab: ElementRef;
   
-  constructor(private storageService: StorageCartService, private productosservice: ProductosService, private router: Router, private route: ActivatedRoute, private modalCtrl: ModalController) {}
+  constructor(private storageService: StorageCartService, 
+    private productosservice: ProductosService, 
+    private router: Router, 
+    private route: ActivatedRoute, 
+    private modalCtrl: ModalController, 
+    private menu: MenuController){}
 
   async openModal(product) {
     const modal = await this.modalCtrl.create({
@@ -57,8 +64,8 @@ export class marketComponent implements OnInit {
 
   }
   toogleMenu() {
-
-
+    this.menuOpen ? this.menu.open("first") : this.menu.close();
+    this.menuOpen = !this.menuOpen;
   }
   getImgUrl(currentproductUri) {
     return `${IMG_URL.productsUri}${currentproductUri}`;
--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.component.html',
-  styleUrls: ['./menu.component.scss'],
+  selector: 'main-mio',
+  templateUrl: 'menu-example.html',
+  styleUrls: ['./menu-example.css'],
 })
-export class MenuComponent implements OnInit {
+export class MenuExample {
 
-  constructor() { }
+constructor(private menu: MenuController) { }
 
-  ngOnInit() {}
+  openFirst() {
+    this.menu.enable(true, 'first');
+    this.menu.open('first');
+  }
 
+  openEnd() {
+    this.menu.open('end');
+  }
+
+  openCustom() {
+    this.menu.enable(true, 'custom');
+    this.menu.open('custom');
+  }
 }
